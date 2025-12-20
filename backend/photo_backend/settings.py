471521e1c596ed7f70d413a2d8127c7bf19175cd
YugDalwadi <yugdalwadi@gmail.com>
2025-12-20 17:33:18 +0530

    feat: basic django setup

---
+++ backend/photo_backend/settings.py
@@ -0,0 +1,101 @@
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+BASE_DIR = Path(__file__).resolve().parent.parent  # This is backend/
+
+yes = load_dotenv(BASE_DIR)
+if (not yes):
+    logger.warning(f"Failed to load .env in settings.py. BASE_DIR: {BASE_DIR}")
+
+SECRET_KEY = os.getenv("DJANGO_SECRET", "your-django-secret")
+
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'photo_backend.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'photo_backend.wsgi.application'
+
+
+# Database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "app_db",
+        "USER": "app_user",
+        "PASSWORD": "app_pwd",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
+
+# AUTH
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+
+STATIC_URL = 'static/'
